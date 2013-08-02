2af5705d7d7753068627d3c0aa4d7a6f59d370bc
w1ld <artyomkarpov@gmail.com>
2013-08-02 14:25:37 +0400

    version 0.4.2

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System.Reflection;
-
-[assembly: AssemblyVersion("0.4.1.0")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+﻿ using System.Reflection;
+ [assembly: AssemblyVersion("0.4.2.0")]
+ [assembly: AssemblyFileVersion("0.4.2.0")]
+
